7cd83cccb9e6ccafd1fe70f7df4891e649b91f51
Zac Danson <zacdanson@Zacs-MacBook-Pro.local>
2018-05-11 20:50:15 +0100

    added back node_modules

--- src/app.js
+++ src/app.js
@@ -2,14 +2,8 @@ import React, { Component } from 'react';
 const app = require('./scss/App.scss');
 import { checkAuth } from './actions/auth-actions/auth_actions';
 import { loading } from './actions/main_actions';
-import reducers from './reducers';
-import { compose, createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
 import { Provider, connect} from 'react-redux';
-import reduxReset from 'redux-reset'
 import { autoRehydrate, persistStore, persistCombineReducers } from 'redux-persist'
-import { PersistGate } from 'redux-persist/es/integration/react'
-import storage from 'redux-persist/es/storage';
 const uuid = require('uuid4');
 import Routes from './routes';
 import {   BrowserRouter as Router, Route, Switch } from 'react-router-dom';
